cda6a29819a6c00e89f0359192c61a3f24c0a620
worker4web <worker4web@gmail.com>
2017-07-11 23:28:02 +0300

    add decision by Map

--- src/main/java/TestTask2.java
+++ src/main/java/TestTask2.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestTask2 {
 
@@ -12,6 +14,27 @@ public class TestTask2 {
         String b = args[1].toLowerCase();
 
         System.out.println(compare(a,b));
+
+        System.out.println(compareByMap(a,b));
+    }
+
+    public static boolean compareByMap(String a, String b) {
+        Map<Integer, Integer> mapA = new HashMap<>();
+        if(a.length()!=b.length())
+            return false;
+        else
+        {
+            for(int i=0;i<a.length();i++)
+                mapA.put((int)a.charAt(i), 1);
+
+            for(int i=0;i<b.length();i++) {
+                if(mapA.get((int)b.charAt(i))!=null)
+                    mapA.remove((int)b.charAt(i));
+                else
+                    return false;
+            }
+            return true;
+        }
     }
 
     public static boolean compare(String a, String b) {
